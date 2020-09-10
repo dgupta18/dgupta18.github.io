381a37ec6348841e38eefaca19a6ba8e3026b8df
Divya Gupta <dgupta@tiramisu.local>
2020-09-10 05:04:08 -0400

    adding scroll css changes

---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    $(function () {
+        $(document).scroll(function () {
+            console.log('in');
+
+            // REPLACE NUBMERS W/ LINKS TO THE IDS OF THE PAGES
+
+            // about
+            if ( $(window).scrollTop() < 900) {
+                // make "about" underlined in navigation (#content has class onpage)
+                $('#nav-about').addClass('onpage');
+                // remove from all other pages
+                $('#nav-experience').removeClass('onpage');
+                $('#nav-projects').removeClass('onpage');
+                $('#nav-resume').removeClass('onpage');
+
+                // dot-onpage
+                $('#dot-about').removeClass('dot').addClass('dot-onpage');
+                // remove all other empty dots
+                $('#dot-experience').removeClass('dot-onpage').addClass('dot');
+                $('#dot-projects').removeClass('dot-onpage').addClass('dot');
+                $('#dot-resume').removeClass('dot-onpage').addClass('dot');
+            }
+
+            // experience
+            if ($(window).scrollTop() >= 900 && $(window).scrollTop() < 1800) {
+                // make "experience" underlined in navigation (#content has class onpage)
+                $('#nav-experience').addClass('onpage');
+                // remove from all other pages
+                $('#nav-about').removeClass('onpage');
+                $('#nav-projects').removeClass('onpage');
+                $('#nav-resume').removeClass('onpage');
+                // dot-onpage
+                $('#dot-experience').removeClass('dot').addClass('dot-onpage');
+                // remove all other empty dots
+                $('#dot-about').removeClass('dot-onpage').addClass('dot');
+                $('#dot-projects').removeClass('dot-onpage').addClass('dot');
+                $('#dot-resume').removeClass('dot-onpage').addClass('dot');
+            }
+
+            // projects
+            if ($(window).scrollTop() >= 1800 && $(window).scrollTop() < 2700) {
+                // make "projects" underlined in navigation (#content has class onpage)
+                $('#nav-projects').addClass('onpage');
+                // remove from all other pages
+                $('#nav-about').removeClass('onpage');
+                $('#nav-experience').removeClass('onpage');
+                $('#nav-resume').removeClass('onpage');
+                // dot-onpage
+                $('#dot-projects').removeClass('dot').addClass('dot-onpage');
+                // remove all other empty dots
+                $('#dot-about').removeClass('dot-onpage').addClass('dot');
+                $('#dot-experience').removeClass('dot-onpage').addClass('dot');
+                $('#dot-resume').removeClass('dot-onpage').addClass('dot');
+            }
+
+            // resume
+            if ($(window).scrollTop() >= 2700) {
+                // make "resume" underlined in navigation (#content has class onpage)
+                $('#nav-resume').addClass('onpage');
+                // remove from all other pages
+                $('#nav-about').removeClass('onpage');
+                $('#nav-experience').removeClass('onpage');
+                $('#nav-projects').removeClass('onpage');
+                // dot-onpage
+                $('#dot-resume').removeClass('dot').addClass('dot-onpage');
+                // remove all other empty dots
+                $('#dot-about').removeClass('dot-onpage').addClass('dot');
+                $('#dot-experience').removeClass('dot-onpage').addClass('dot');
+                $('#dot-projects').removeClass('dot-onpage').addClass('dot');
+            }
+        });
+    });
+});
